552735a93b00f0e3ce1c3b5a1f5b5066dd992842
piyush-shaw <piyushshaw0000@gmail.com>
2023-03-01 15:16:25 +0530

    [Piyush] added to maintain list of company share using linked list - stack and queue

---
+++ ObjectOrientedUsingJson/ObjectOrientedUsingJson/Json/AccountUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ObjectOrientedUsingJson.Json
+{
+	public class AccountUtility
+	{
+        public List<Account> AccountList { get; set; }
+
+        public class Account
+        {
+            public string Stockholder { get; set; }
+            public string StockName { get; set; }
+            public int shares { get; set; }
+            public int Price { get; set; }
+        }
+    }
+}
+
---
+++ ObjectOrientedUsingJson/ObjectOrientedUsingJson/Json/StockUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ObjectOrientedUsingJson.Json
+{
+	public class StockUtility
+	{
+        public LinkedList<Stocks> stocksList = new LinkedList<Stocks>();
+
+        public class Stocks
+        {
+            public string StockName { get; set; }
+            public int shares { get; set; }
+            public int Price { get; set; }
+        }
+    }
+}
+
